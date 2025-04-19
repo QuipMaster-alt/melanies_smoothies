02095c5a203faaa0c3a678660762af5e0e90ac77
QuipMaster-alt <luispmartinez@icloud.com>
2025-04-19 16:12:18 -0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -4,6 +4,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import pandas as pd
 
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
