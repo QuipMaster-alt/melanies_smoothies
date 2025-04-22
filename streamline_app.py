ace9873c4602a40149153bce5d0a8356f1775789
QuipMaster-alt <luispmartinez@icloud.com>
2025-04-21 21:38:17 -0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,18 +1,19 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
 
-name_on_order = st.text_input("Name on Smoothie:")
+name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
@@ -54,5 +55,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+        st.success(name_on_order + 'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
